6fa86140859f560b12b7c369303fa8c712f3f960
MaxRamgraber <37251290+MaxRamgraber@users.noreply.github.com>
2018-11-28 10:47:26 +0100

    Update threex-armarkercontrols.js

--- threex/threex-armarkercontrols.js
+++ threex/threex-armarkercontrols.js
@@ -187,6 +187,8 @@ ARjs.MarkerControls.prototype._initArtoolkit = function(){
 		// check if arController is init
 		var arController = _this.context.arController
 		console.assert(arController !== null )
+		
+		arController.setPattRatio(0.7);
 
 		// start tracking this pattern
 		if( _this.parameters.type === 'pattern' ){
